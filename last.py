bf4d6b779ce39a44efcee863a84f31b1756b25be
madara21566 <madaraop2156@gmail.com>
2025-06-21 22:44:30 +0530

    Delete last.pytelegram.

--- last.py
+++
@@ -1,9 +0,0 @@
-
-from telegram import Update
-from telegram.ext import ContextTypes
-
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Bot is alive!")
-
-OWNER_ID = 123456789  # replace with your ID
-GROUP_ADMINS = set()
