f9812480ae918323edd197fd265aecc60c1ca603
madara21566 <madaraop2156@gmail.com>
2025-06-27 18:50:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
 from flask import Flask
-import asyncio
-import threading
-from bot_runner import run_bot
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "✅ Bot + Website both running!"
-
-def start_bot():
-    asyncio.run(run_bot())
-
-threading.Thread(target=start_bot, daemon=True).start()
-
-# Flask will run through Gunicorn
+    return "✅ Website is running!"
