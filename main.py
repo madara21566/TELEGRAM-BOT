234af6617d8522ef4600ee05aba6db98e1bfb0ab
madara21566 <madaraop2156@gmail.com>
2025-06-27 15:01:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,31 @@
-import os
-from telegram.ext import Application, CommandHandler, MessageHandler, CallbackQueryHandler, filters
+from telegram.ext import ApplicationBuilder
 from NIKALLLLLLL import (
     start, set_filename, set_contact_name, set_limit, set_start,
     set_vcf_start, make_vcf_command, merge_command, done_merge,
     export_users, owner_panel, handle_callback, handle_owner_input,
     handle_document, handle_text
 )
+from telegram.ext import CommandHandler, MessageHandler, CallbackQueryHandler, filters
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-BOT_USERNAME = os.environ.get("BOT_USERNAME")
-WEBHOOK_URL = f"https://{os.environ.get('RENDER_EXTERNAL_HOSTNAME')}/{BOT_USERNAME}"
+TOKEN = "7869581039:AAGWWs3d75a0PXjCwG59JFDtqkkPicuRPWQ"
 
-application = Application.builder().token(BOT_TOKEN).build()
+app = ApplicationBuilder().token(TOKEN).build()
 
-application.add_handler(CommandHandler("start", start))
-application.add_handler(CommandHandler("setfilename", set_filename))
-application.add_handler(CommandHandler("setcontactname", set_contact_name))
-application.add_handler(CommandHandler("setlimit", set_limit))
-application.add_handler(CommandHandler("setstart", set_start))
-application.add_handler(CommandHandler("setvcfstart", set_vcf_start))
-application.add_handler(CommandHandler("makevcf", make_vcf_command))
-application.add_handler(CommandHandler("merge", merge_command))
-application.add_handler(CommandHandler("done", done_merge))
-application.add_handler(CommandHandler("exportusers", export_users))
-application.add_handler(CommandHandler("panel", owner_panel))
-application.add_handler(CallbackQueryHandler(handle_callback))
-application.add_handler(MessageHandler(filters.Document.ALL, handle_document))
-application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_owner_input))
-application.add_handler(MessageHandler(filters.TEXT, handle_text))
+app.add_handler(CommandHandler("start", start))
+app.add_handler(CommandHandler("setfilename", set_filename))
+app.add_handler(CommandHandler("setcontactname", set_contact_name))
+app.add_handler(CommandHandler("setlimit", set_limit))
+app.add_handler(CommandHandler("setstart", set_start))
+app.add_handler(CommandHandler("setvcfstart", set_vcf_start))
+app.add_handler(CommandHandler("makevcf", make_vcf_command))
+app.add_handler(CommandHandler("merge", merge_command))
+app.add_handler(CommandHandler("done", done_merge))
+app.add_handler(CommandHandler("exportusers", export_users))
+app.add_handler(CommandHandler("panel", owner_panel))
+app.add_handler(CallbackQueryHandler(handle_callback))
+app.add_handler(MessageHandler(filters.Document.ALL, handle_document))
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_owner_input))
+app.add_handler(MessageHandler(filters.TEXT, handle_text))
 
 if __name__ == "__main__":
-    application.run_webhook(
-        listen="0.0.0.0",
-        port=5000,
-        url_path=BOT_USERNAME,
-        webhook_url=WEBHOOK_URL
-    )
+    app.run_polling()
