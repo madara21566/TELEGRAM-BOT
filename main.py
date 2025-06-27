154561925df18f6f0ec0e86284f4f96ed76e8721
madara21566 <madaraop2156@gmail.com>
2025-06-27 14:57:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,18 @@
 import os
-from telegram.ext import (
-    Application,
-    CommandHandler,
-    MessageHandler,
-    CallbackQueryHandler,
-    ContextTypes,
-    filters,
-)
+from telegram.ext import Application, CommandHandler, MessageHandler, CallbackQueryHandler, filters
 from NIKALLLLLLL import (
-    start,
-    set_filename,
-    set_contact_name,
-    set_limit,
-    set_start,
-    set_vcf_start,
-    make_vcf_command,
-    merge_command,
-    done_merge,
-    export_users,
-    owner_panel,
-    handle_callback,
-    handle_owner_input,
-    handle_document,
-    handle_text
+    start, set_filename, set_contact_name, set_limit, set_start,
+    set_vcf_start, make_vcf_command, merge_command, done_merge,
+    export_users, owner_panel, handle_callback, handle_owner_input,
+    handle_document, handle_text
 )
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 BOT_USERNAME = os.environ.get("BOT_USERNAME")
-
-WEBHOOK_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
-WEBHOOK_URL = f"https://{WEBHOOK_HOSTNAME}/{BOT_USERNAME}"
+WEBHOOK_URL = f"https://{os.environ.get('RENDER_EXTERNAL_HOSTNAME')}/{BOT_USERNAME}"
 
 application = Application.builder().token(BOT_TOKEN).build()
 
-# Handlers
 application.add_handler(CommandHandler("start", start))
 application.add_handler(CommandHandler("setfilename", set_filename))
 application.add_handler(CommandHandler("setcontactname", set_contact_name))
