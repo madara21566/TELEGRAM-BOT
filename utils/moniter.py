3f1b04167d0644acf4979bc932c5dd4745a3ceb7
madara21566 <madaraop2156@gmail.com>
2025-11-10 10:36:52 +0530

    Update moniter.py

--- utils/moniter.py
+++ utils/moniter.py
@@ -1,23 +1,57 @@
-import time, os, signal
+import time, os
 from utils.helpers import load_json, save_json, STATE_FILE
+from utils.runner import stop_script, start_script
+
+# Free user limits
+FREE_PROJECT_LIMIT = 2
+FREE_RUNTIME_LIMIT = 12 * 3600  # 12 hours in seconds
+
+# Premium user limits
+PREMIUM_PROJECT_LIMIT = 10
+PREMIUM_RUNTIME_LIMIT = None  # Unlimited (24/7)
 
 def check_expired():
-    st = load_json()
-    changed = False
-    now = time.time()
-    for uid, procs in list(st.get("procs", {}).items()):
-        for key, meta in list(procs.items()):
-            exp = meta.get("expire")
-            if exp and now > exp:
-                pid = meta.get("pid")
-                try:
-                    os.killpg(os.getpgid(pid), signal.SIGTERM)
-                except Exception:
-                    pass
-                del procs[key]
-                changed = True
-        if uid in st.get("procs", {}) and not st["procs"][uid]:
-            del st["procs"][uid]
-            changed = True
-    if changed:
-        save_json(STATE_FILE, st)
+    state = load_json()
+    users = state.get("users", {})
+    procs = state.get("procs", {})
+
+    for uid, projects in procs.items():
+        uid_str = str(uid)
+
+        # Is user premium?
+        is_premium = users.get(uid_str, {}).get("premium", False)
+
+        # Set correct limits
+        project_limit = PREMIUM_PROJECT_LIMIT if is_premium else FREE_PROJECT_LIMIT
+        runtime_limit = PREMIUM_RUNTIME_LIMIT if is_premium else FREE_RUNTIME_LIMIT
+
+        # Check project count
+        user_projects = users.get(uid_str, {}).get("projects", [])
+        if len(user_projects) > project_limit:
+            extra = user_projects[project_limit:]
+            for project in extra:
+                stop_script(uid_str, project)
+
+        # Check runtime expiration
+        for proj_key, info in list(projects.items()):
+            proj = proj_key.split(":")[0]
+            start_time = info.get("start", 0)
+
+            if runtime_limit and start_time and (time.time() - start_time > runtime_limit):
+                stop_script(uid_str, proj)
+
+    save_json(STATE_FILE, state)
+
+
+def restore_processes_after_restart():
+    state = load_json()
+    procs = state.get("procs", {})
+
+    for uid, projects in procs.items():
+        for proj_key, info in projects.items():
+            proj = proj_key.split(":")[0]
+            cmd = info.get("cmd", None)
+            try:
+                start_script(uid, proj, cmd)
+            except:
+                pass
