03b577bd735b2f2bc9d966568616b6facef3ae50
madara21566 <madaraop2156@gmail.com>
2025-11-10 10:19:06 +0530

    Create moniter.py

---
+++ utils/moniter.py
@@ -0,0 +1,21 @@
+# utils/monitor.py
+import time, os, signal
+from utils.helpers import load_json, save_json, STATE_FILE
+
+def check_expired():
+    st = load_json()
+    changed = False
+    now = time.time()
+    for uid, procs in list(st.get("procs", {}).items()):
+        for key, meta in list(procs.items()):
+            exp = meta.get("expire")
+            if exp and now > exp:
+                pid = meta.get("pid")
+                try:
+                    os.killpg(os.getpgid(pid), signal.SIGTERM)
+                except Exception:
+                    pass
+                del procs[key]
+                changed = True
+    if changed:
+        save_json(STATE_FILE, st)
